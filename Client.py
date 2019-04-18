2655cc3bdc8b9b0f9ff4767f6161f5aa3fbfe60e
lavih <lavih44@walla.co.il>
2019-04-18 11:05:20 +0300

    new

--- Client.py
+++ Client.py
@@ -1,6 +1,5 @@
 import socket
 print "start:"
-
 ##open socket, send and recieve data, close socket ##
 user_socket = socket.socket()
 user_socket.connect(('127.0.0.1', 1729))
@@ -14,10 +13,13 @@ while flag == 0:
   print "keep session with SERVER (y/n)?" 
   ans = raw_input()
   if ans == 'y':
-   print "Please enter another msg..."
+   print "Please enter another msg..." 
    user_input = raw_input()
+   user_socket = socket.socket()
+   user_socket.connect(('127.0.0.1', 1729))
    user_socket.send(user_input)
    data = user_socket.recv(1024)
+   print data
   else :
    flag = 1
    user_socket.close()
--- Server.py
+++ Server.py
@@ -6,13 +6,13 @@ print "Waiting for data from Client..."
 ##creating server ##
 server_socket = socket.socket()
 server_socket.bind(('0.0.0.0', 1729))
-server_socket.listen(1)
+server_socket.listen(2)
 (client_socket, client_address) = server_socket.accept()
-client_name = client_socket.recv(1024)
-print "data the client sent:" + client_name
-print "Sending to client the following msg: connected to server, data sent to server -" + client_name
-client_socket.send("connected to server, data sent to server - " + client_name)
-
+client_data = client_socket.recv(1024)
+print "data the client sent:" + client_data
+print "Client-Data = " +  str( tuple(client_address) )
+print "Sending to client the following msg: connected to server, data sent to server -" + client_data
+client_socket.send("connected to server, data sent to server - " + client_data)
 
 flag = 0
 while flag == 0:
@@ -20,15 +20,19 @@ while flag == 0:
  ans = raw_input()
  if ans == 'y' :
    print "Waiting for data from Client..."
-   server_socket.listen(1)
+   server_socket.listen(2)
    (client_socket, client_address) = server_socket.accept()
-   client_name = client_socket.recv(1024)
-   print "data the client sent:" + client_name
+   client_data = client_socket.recv(1024)
+   print "data the client sent:" + client_data
+   print "Client-Data = " +  str( tuple(client_address) )
+   print "Sending to client the following msg: connected to server, data sent to server -" + client_data
+   client_socket.send("connected to server, data sent to server - " + client_data)
  else:
   flag = 1
   client_socket.close()
 #here we choose to end the session with the client and can connect another client#
 
-
-
 server_socket.close()
+
+
+## serverrr
